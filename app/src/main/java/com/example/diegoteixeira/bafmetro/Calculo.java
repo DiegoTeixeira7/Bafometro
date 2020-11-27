abc69a92a17bf3ef5221b71351508c7dcc3d4311
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-11-27 13:22:47 -0300

    frontend tela2 finalizado

---
+++ app/src/main/java/com/example/diegoteixeira/bafmetro/Calculo.java
@@ -0,0 +1,18 @@
+package com.example.diegoteixeira.bafmetro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Calculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo);
+    }
+
+    public void calcular(View view) {
+    }
+}
